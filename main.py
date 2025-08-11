4f0bc3687d01190fc628fa76aa033656a3bc7fba
ermerga <ermerga@gmail.com>
2025-08-10 21:25:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://ermerga.github.io"])
+
 
 # Function to load or fetch S&P 500 data
 def load_sp500_data(ticker):
